f87f47fb7ff0fb01fc655742ebed157ad7345b72
Severiano Atencio <satencio@consultec-ti.com>
2021-09-26 15:51:17 -0500

    change: refactor handler

--- handler/habit_handler.go
+++ handler/habit_handler.go
@@ -10,16 +10,17 @@ import (
 	"github.com/isaacRevan24/gamification-toolkit-logic/utility"
 )
 
-func signUp(context *gin.Context) {
-	var signUpRequest model.SignUpRequest
-	mapping := utility.GenericRequestJsonMapper(&signUpRequest, context)
+func addHabit(context *gin.Context) {
+	var addNewHabitRequest model.AddNewHabitRequest
+	mapping := utility.GenericRequestJsonMapper(&addNewHabitRequest, context)
 	if mapping != nil {
 		// TODO: hacer paquete de logs
 		var Error = log.New(os.Stdout, "\u001b[31mERROR: \u001b[0m", log.LstdFlags|log.Lshortfile)
 		Error.Println(mapping)
+
 		return
 	}
 	var controller controller.UserControllerInterface = controller.NewUserController()
-	response := controller.SignUpController(signUpRequest)
+	response := controller.AddNewHabitController(addNewHabitRequest)
 	context.JSON(getHttpStatusByCode(response.Status.Code), response)
 }
