bfc2265c5aa25264f791e976754e033b6643da65
Severiano Atencio <satencio@consultec-ti.com>
2021-09-26 21:55:25 -0500

    add: Logging

--- handler/habit_handler.go
+++ handler/habit_handler.go
@@ -8,13 +8,19 @@ import (
 )
 
 func addHabit(context *gin.Context) {
+	const functionName string = "addHabit"
+	Logs.LogDebug("Start " + functionName)
+
 	var addNewHabitRequest model.AddNewHabitRequest
 	err := utility.GenericRequestJsonMapper(&addNewHabitRequest, context)
 	if err != nil {
 		Logs.LogError(err)
 		return
 	}
+
 	var controller controller.HabitControllerInterface = controller.NewHabitController()
 	response := controller.AddNewHabitController(addNewHabitRequest)
+
+	Logs.LogDebug("End " + functionName)
 	context.JSON(getHttpStatusByCode(response.Status.Code), response)
 }
--- handler/user_hadler.go
+++ handler/user_hadler.go
@@ -8,13 +8,20 @@ import (
 )
 
 func signUp(context *gin.Context) {
+	const functionName string = "signUp"
+	Logs.LogDebug("Start " + functionName)
+
 	var signUpRequest model.SignUpRequest
 	err := utility.GenericRequestJsonMapper(&signUpRequest, context)
+
 	if err != nil {
 		Logs.LogError(err)
 		return
 	}
+
 	var controller controller.UserControllerInterface = controller.NewUserController()
 	response := controller.SignUpController(signUpRequest)
+
+	Logs.LogDebug("End " + functionName)
 	context.JSON(getHttpStatusByCode(response.Status.Code), response)
 }
