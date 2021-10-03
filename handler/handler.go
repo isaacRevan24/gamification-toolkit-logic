51efc01f212f09905b9c6a3e972edf375eace390
Severiano Atencio <satencio@consultec-ti.com>
2021-10-03 16:53:00 -0500

    change: remove status builder

--- handler/handler.go
+++ handler/handler.go
@@ -2,11 +2,8 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 	"github.com/isaacRevan24/gamification-toolkit-logic/controller"
-	"github.com/isaacRevan24/gamification-toolkit-logic/model"
 	"github.com/isaacRevan24/gamification-toolkit-logic/repository"
 	"github.com/isaacRevan24/gamification-toolkit-logic/utility"
 )
@@ -37,17 +34,4 @@ func NewHabitHandler() HabitHandlerInterface {
 }
 func NewUserHandler() UserHandlerInterface {
 	return &userHandler{}
-}
-
-func getHttpStatusByCode(code string) int {
-	switch code {
-	case model.SUCCESS_CODE_STATUS:
-		return http.StatusOK
-	case model.BAD_REQUEST_ERROR_STATUS:
-		return http.StatusBadRequest
-	case model.INTERNAL_SERVER_ERROR_STATUS:
-		return http.StatusInternalServerError
-	default:
-		return http.StatusBadRequest
-	}
 }
