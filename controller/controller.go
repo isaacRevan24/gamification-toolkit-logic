b0b6e6ad454a0d9b26b4af5eeaa8cef894d5798e
Severiano Atencio <satencio@consultec-ti.com>
2021-10-16 15:31:15 -0500

    Refator de paquetes a un file por simplicidad

--- controller/controller.go
+++ controller/controller.go
@@ -34,3 +34,73 @@ func NewHabitController(repo repository.HabitRepository) HabitControllerInterfac
 	habitRepository = repo
 	return &habitController{}
 }
+
+func (*userController) SignUp(request model.SignUpRequest) error {
+	const functionName string = "SignUpController"
+	Logs.LogDebug("Start " + functionName)
+
+	err := userRepository.SignUpRepository(request.ID)
+
+	if err != nil {
+		Logs.LogDebug("End " + functionName)
+		return err
+	}
+
+	Logs.LogInfo("Successfully saved user.")
+	Logs.LogDebug("End " + functionName)
+
+	return nil
+}
+
+func (*habitController) AddNewHabit(reques model.AddNewHabitRequest) (int, error) {
+	const functionName string = "AddNewHabitController"
+	Logs.LogDebug("Start " + functionName)
+
+	conditionError := validateHabitCondition(reques.Condition)
+
+	if conditionError != nil {
+		Logs.LogError(conditionError)
+		Logs.LogDebug("End " + functionName)
+		return 0, conditionError
+	}
+
+	habitId, err := habitRepository.AddNewHabitRepository(reques.UserId, reques.Name, reques.Description, reques.Condition, reques.Repetition)
+
+	if err != nil {
+		Logs.LogDebug("End " + functionName)
+		return 0, err
+	}
+
+	Logs.LogInfo("New habit created")
+	Logs.LogDebug("End " + functionName)
+
+	return habitId, nil
+}
+
+func (*habitController) DeleteHabit(userId string, habitId string) (bool, error) {
+	const functionName string = "DeleteHabit"
+	Logs.LogDebug("Start " + functionName)
+
+	response, err := habitRepository.DeleteHabitRepository(userId, habitId)
+
+	if err != nil {
+		Logs.LogDebug("End " + functionName)
+		return false, err
+	}
+
+	Logs.LogDebug("End " + functionName)
+	return response, nil
+}
+
+func validateHabitCondition(condition string) error {
+
+	conditionTypes := [3]string{"D", "W", "Y"}
+
+	for _, i := range conditionTypes {
+		if condition == i {
+			return nil
+		}
+	}
+
+	return ErrorInvalidHabitCondition
+}
--- handler/handler.go
+++ handler/handler.go
@@ -2,8 +2,12 @@
 package handler
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/isaacRevan24/gamification-toolkit-logic/controller"
+	"github.com/isaacRevan24/gamification-toolkit-logic/model"
 	"github.com/isaacRevan24/gamification-toolkit-logic/repository"
 	"github.com/isaacRevan24/gamification-toolkit-logic/utility"
 )
@@ -37,4 +41,124 @@ func NewHabitHandler() HabitHandlerInterface {
 
 func NewUserHandler() UserHandlerInterface {
 	return &userHandler{}
+}
+
+func (*userHandler) SignUp(context *gin.Context) {
+	const functionName string = "signUp"
+	Logs.LogDebug("Start " + functionName)
+
+	var signUpRequest model.SignUpRequest
+	var response model.SignUpResponse
+	mapperError := mapper.GenericRequestJsonMapper(&signUpRequest, context)
+
+	if mapperError != nil {
+		Logs.LogError(mapperError)
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Missing arguments."})
+		return
+	}
+
+	signUpError := userController.SignUp(signUpRequest)
+
+	if signUpError != nil {
+		response.Status = mapper.StatusBuilder(model.BAD_REQUEST_ERROR_STATUS, "Error saving the user")
+		context.JSON(http.StatusBadRequest, response)
+
+		Logs.LogError(signUpError)
+		Logs.LogDebug("End " + functionName)
+		return
+	}
+
+	response.Status = mapper.StatusBuilder(model.SUCCESS_CODE_STATUS, "Successfully saved user.")
+
+	Logs.LogDebug("End " + functionName)
+	context.JSON(http.StatusOK, response)
+}
+
+func (*habitHandler) AddHabit(context *gin.Context) {
+	const functionName string = "AddHabit"
+	Logs.LogDebug("Start " + functionName)
+
+	var addNewHabitRequest model.AddNewHabitRequest
+	var response model.AddNewHabitResponse
+
+	mappingError := mapper.GenericRequestJsonMapper(&addNewHabitRequest, context)
+
+	if mappingError != nil {
+		Logs.LogError(mappingError)
+		context.JSON(http.StatusBadRequest, gin.H{"error": "Missing arguments."})
+		return
+	}
+
+	habitId, controllerError := habitController.AddNewHabit(addNewHabitRequest)
+
+	if controllerError == controller.ErrorInvalidHabitCondition {
+
+		response.Status = mapper.StatusBuilder(model.BAD_REQUEST_ERROR_STATUS, "Invalid habit condition.")
+
+		Logs.LogError(controllerError)
+		context.JSON(http.StatusBadRequest, response)
+		return
+
+	} else if controllerError != nil {
+
+		response.Status = mapper.StatusBuilder(model.BAD_REQUEST_ERROR_STATUS, "Error creating new habit.")
+
+		Logs.LogError(controllerError)
+		context.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response.Status = mapper.StatusBuilder(model.SUCCESS_CODE_STATUS, "New habit created")
+	response.HabitId = habitId
+
+	Logs.LogDebug("End " + functionName)
+
+	context.JSON(http.StatusOK, response)
+}
+
+func (*habitHandler) DeleteHabit(context *gin.Context) {
+	const functionName string = "DeleteHabit"
+	Logs.LogDebug("Start " + functionName)
+
+	habitId := context.Param("id")
+	userId := context.Param("user")
+	Logs.LogDebug("Habit id: " + habitId)
+	Logs.LogDebug("user id: " + userId)
+
+	var response model.DeleteHabitResponse
+
+	isDeleted, controllerError := habitRepository.DeleteHabitRepository(userId, habitId)
+
+	if controllerError != nil {
+		response.Status = mapper.StatusBuilder(model.BAD_REQUEST_ERROR_STATUS, "Error deleting habit.")
+		Logs.LogError(controllerError)
+		Logs.LogDebug("End " + functionName)
+		context.JSON(http.StatusInternalServerError, response)
+		return
+	}
+
+	if isDeleted {
+		response.Status = mapper.StatusBuilder(model.SUCCESS_CODE_STATUS, "Habit deleted.")
+		Logs.LogDebug("End " + functionName)
+		context.JSON(http.StatusOK, response)
+		return
+	} else {
+		response.Status = mapper.StatusBuilder(model.BAD_REQUEST_ERROR_STATUS, "Habit already deleted.")
+		Logs.LogDebug("End " + functionName)
+		context.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+}
+
+func (*habitHandler) CheckHabit(context *gin.Context) {
+	var headers model.CheckHabitHeaders
+
+	if err := context.ShouldBindHeader(&headers); err != nil {
+		context.JSON(200, err)
+	}
+
+	fmt.Println(headers)
+
+	context.JSON(http.StatusOK, gin.H{"chech habit": "ok"})
 }
